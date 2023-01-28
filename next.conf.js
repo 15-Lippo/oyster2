ff5100e1ae6bb2d690dde0be5ec73adf4fbacf8c
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-01-28 09:05:38 +0100

    Create next.conf.js

---
+++ next.conf.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */ 
+ const nextConfig = { 
+   reactStrictMode: true, 
+   webpack: (config, { isServer }) => { 
+     config.resolve.fallback = { 
+       "@solana/spl-token-swap": require.resolve("@solana/spl-token-swap"), 
+       crypto: false, 
+       stream: false, 
+     }; 
+  
+     return config; 
+   }, 
+   typescript: { 
+     // !! WARN !! 
+     // Dangerously allow production builds to successfully complete even if 
+     // your project has type errors. 
+     // !! WARN !! 
+     // Only ignoring this to resolve Type errors in @monaco-protocol/admin-client package 
+     // Delete this when not building with the above package. 
+     ignoreBuildErrors: true, 
+   }, 
+ }; 
+  
+ module.exports = nextConfig;
